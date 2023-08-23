ef4836b4326bb5424432a7cf67bd9c0d6ebc6acc
Mmadineo <mmadineo.seleke@gmail.com>
2023-08-23 03:05:52 -0700

    pass checks

--- my_pall.c
+++ my_pall.c
@@ -5,14 +5,15 @@
 * @counter: no used
 * Return: no return
 */
+
 void f_pall(stack_t **head, unsigned int counter)
 {
-stack_t *temp;
-(void)counter;
+	stack_t *temp;
+	(void)counter;
 
-if (*head == NULL)
-return;
+	if (*head == NULL)
+		return;
 
-for (temp = *head; temp; temp = temp->next)
-printf("%d\n", temp->n);
+	for (temp = *head; temp; temp = temp->next)
+		printf("%d\n", temp->n);
 }
